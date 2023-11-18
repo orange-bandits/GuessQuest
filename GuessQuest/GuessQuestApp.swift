6cdeba63a5120499858451d37e2d12f6c0965205
Tukan van der Borgh <tukanvdborgh@proton.me>
2023-11-18 16:44:37 +0100

    Initial Commit

---
+++ GuessQuest/GuessQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessQuestApp.swift
+//  GuessQuest
+//
+//  Created by Tukan van der Borgh on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
